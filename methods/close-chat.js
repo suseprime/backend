e8a2d2e5cee64190127bec95aa1d5a1f7d7533fc
Vojta Staněk <stanekv01@gmail.com>
2015-01-11 21:59:20 +0100

    Adding close chat method

---
+++ methods/close-chat.js
@@ -0,0 +1,18 @@
+data = require('../data.js');
+
+exports.type = 'close-chat';
+exports.requiredParams = ['chat-id'];
+exports.signInRequired = true;
+exports.callback = function (params, user) {
+  var chat = data.chats[params['chat-id']];
+
+  if(chat.from == this.id) {
+    data.sockets[chat.to].sendMessage({'type':'chat-closed', 'chat-id':id});
+  } else if (chat.to === this.id) {
+    data.sockets[chat.from].sendMessage({'type':'chat-closed', 'chat-id':id});
+  }
+  
+  delete data.chats[id];
+
+  user.sendMessage({'type': 'close-chat-success', 'request-id': params['request-id']});
+};
