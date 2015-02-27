9ecafa0d70320dc617512a853dd66bc3d8d73a49
Vojta Staněk <stanekv01@gmail.com>
2015-02-27 16:01:41 +0100

    Fix chat close bug

--- methods/close-chat.js
+++ methods/close-chat.js
@@ -5,13 +5,13 @@ exports.requiredParams = ['chat-id'];
 exports.signInRequired = true;
 exports.callback = function (params, user) {
   var chat = data.chats[params['chat-id']];
-
+  var id = params['chat-id'];
   if(chat.from == this.id) {
     data.sockets[chat.to].sendMessage({'type':'chat-closed', 'chat-id':id});
   } else if (chat.to === this.id) {
     data.sockets[chat.from].sendMessage({'type':'chat-closed', 'chat-id':id});
   }
-  
+
   delete data.chats[id];
 
   user.sendMessage({'type': 'close-chat-success', 'request-id': params['request-id']});
